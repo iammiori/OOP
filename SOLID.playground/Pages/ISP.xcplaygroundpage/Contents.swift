e1cde070eb586706dedbb7930fea6f6d7ffac3af
Mi Yeon Lee <immioriii@gmail.com>
2022-11-08 23:45:41 +0900

    feat: ISP 인터페이스 분리원칙

---
+++ SOLID.playground/Pages/ISP.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//MARK: ISP (인터페이스 분리 원칙)
+/*
+ - 덩치가 큰 protocol을 상속 받으면 불필요한 메소드를 선언해야 할 가능성이 높음
+ - ex)  - UITableViewDataSource와 UITableViewDelegate 분리
+ */
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
